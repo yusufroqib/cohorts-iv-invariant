de819f7a8964da0abab9c5f14f29d3b36276f502
enniwealth <wealththefirstx1000@gmail.com>
2025-01-22 16:16:49 +0100

    New Push

---
+++ test/StatelessFuzzCatchesTest.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {StatelessFuzzCatches} from "../../src/StatelessFuzzCatches.sol";
+
+contract StatelessFuzzCatchesTest is Test {
+    StatelessFuzzCatches public sfc;
+
+    function setUp() public {
+        sfc = new StatelessFuzzCatches();
+    }
+
+    // function testFuzzCatchesBugStateless() public view {
+    //     uint128 rng = 2;
+    //     assert(sfc.doMath(rng) != 0);
+    // }
+
+    // // Stateless fuzz
+    // // Uncomment this and you'll see it catches the bug!
+    function testFuzzCatchesBugStateless(uint128 randomNumber) public view {
+        assert(sfc.doMath(randomNumber) != 0);
+    }
+}
